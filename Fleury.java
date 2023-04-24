7a9b5707055436332a151d1e395736bb9ffb9e24
rosskogel <89945021+rosskogel@users.noreply.github.com>
2023-04-24 07:27:57 -0500

    Update Fleury.java

--- Fleury.java
+++ Fleury.java
@@ -1,5 +1,34 @@
+import java.util.ArrayList;
+
 public class Fleury {
-    
+    ArrayList<Node> graph = new ArrayList<Node>();
+
+    public Node findStartVert(ArrayList<Node> graph) {
+        for (Node n: graph) {
+            int deg = n.getAdjacencyList().size();
+            if (deg % 2 == 1) {
+                return n;
+            }
+        }
+        return null;
+    }
+
+    public int dfs(Node prev, Node start, ArrayList<Node> visited){
+        int count = 1;
+        if (!(visited.contains(start))) {
+            visited.add(start);
+        }
+        for (Node n: graph) {
+            if (!(prev == n)) {
+                if (!(visited.contains(n))) {
+                    if (n.getAdjacencyList().contains(start)) {
+                        count = count + dfs(start, n, visited);
+                    }
+                }
+            }
+        }
+        return count;
+    }
 
     public boolean isBridge(Node start, Node end){
         int degree = 0;
@@ -11,5 +40,4 @@ public class Fleury {
         }
         return true;
     }
-
 }
